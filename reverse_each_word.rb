9b64e96447c207ccf6e43fdd2f1bef0d15d61780
Aliya Merali <aliya.merali@flatironschool.com>
2020-05-25 17:12:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,7 +11,7 @@
 
 def reverse_each_word(string)
   array = string.split(" ")
-  array.collect {|word| word.reverse}.join(" ")
+  string.split.collect {|word| word.reverse}.join(" ")
 end
 
 # reverse_each_word("Hello there, and how are you?")
